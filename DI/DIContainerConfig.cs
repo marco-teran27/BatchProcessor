bf42cbe1ea3444b2677516064e801e6cfacaaa6d
marco.teran <marco.teran@earlens.com>
2025-02-23 19:11:47 -0800

    updated both test csprojs for lang 10 - this completes all of the errors relating to net48 conversion of the initial implementation
    build is successfull

--- DI/DIContainerConfig.cs
+++ DI/DIContainerConfig.cs
@@ -1,5 +1,6 @@
 // File: DI\DIContainerConfig.cs
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using Batch.Core; // For TheOrchestrator
 using Commons.Interfaces; // For ITheOrchestrator, IRhinoCommOut
 using ConfigJSON; // For ConfigSelector, ConfigParser
@@ -8,6 +9,7 @@ namespace DI
 {
     public static class DIContainerConfig
     {
+#pragma warning disable IDE0060 // Suppress unused parameter warning
         public static IServiceProvider ConfigureServices(bool useRhino = true)
         {
             var services = new ServiceCollection();
